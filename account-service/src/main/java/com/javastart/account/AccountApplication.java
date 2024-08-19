a5b21ecb05d4e62b8e68af482ceabb46ec44031d
Lesha-Chinikailo <lesha2004.0000@gmail.com>
2024-08-19 15:39:47 +0300

    added docker in account-service

--- account-service/src/main/java/com/javastart/account/AccountApplication.java
+++ account-service/src/main/java/com/javastart/account/AccountApplication.java
@@ -3,9 +3,11 @@ package com.javastart.account;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableJpaRepositories("com.javastart.account.repository")
 public class AccountApplication {
 
     public static void main(String[] args) {
